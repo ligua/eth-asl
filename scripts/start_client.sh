b9a9ff76c99c592025b1dc6a521e65ab7cd6bf63
taivop <taivo.pungas@gmail.com>
2016-09-23 13:11:52 +0200

    testing now with single memslap thread, works (prints out result)

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,3 +1,5 @@
 echo "Starting a memaslap client."
-mem/libmemcached-1.0.18/clients/memaslap -s localhost:11212 -T 64 -c 64 -o1 -S 1s -t 10s # -F <workloadconfig>
+mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 1 -c 1 -o1 -S 1s -x 10 -F conf/client_workload.config
+
+# mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o1 -S 1s -t 10s -F conf/client_workload.config
 # -s <server>:11212 -T 64 -c 64 -o1 -S 1s -t <time> -F <workloadconfig>
