ad42a95a930df61660c6f48b163dda4391abc765
taivop <taivo.pungas@gmail.com>
2016-09-23 17:18:01 +0200

    work on #3

--- src/main/java/asl/LoadBalancer.java
+++ src/main/java/asl/LoadBalancer.java
@@ -8,7 +8,7 @@ import java.util.List;
 /**
  * This is the class that takes all incoming requests, hashes them and forwards to the correct MiddlewareComponent(s).
  */
-public class LoadBalancer {
+public class LoadBalancer extends Thread {
 
     private static final Logger log = LogManager.getLogger(LoadBalancer.class);
 
@@ -28,6 +28,23 @@ public class LoadBalancer {
 
     void handleReadRequest() {
 
+    }
+
+    /**
+     * Find if the request was a get, set or delete request.
+     */
+    public static RequestType getRequestType(String message) {
+        String firstThreeChars = message.substring(0, 3);
+
+        if(firstThreeChars.equals("set")) {
+            return RequestType.SET;
+        } else if(firstThreeChars.equals("get")) {
+            return RequestType.GET;
+        } else if(firstThreeChars.equals("del")) {
+            return RequestType.DELETE;
+        }
+
+        return RequestType.OTHER;
     }
 
 }
---
+++ src/main/java/asl/Request.java
@@ -0,0 +1,7 @@
+package main.java.asl;
+
+enum RequestType { GET, SET, DELETE, OTHER };
+
+public class Request {
+
+}
