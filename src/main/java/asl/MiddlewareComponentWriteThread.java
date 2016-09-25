39df91e92fe4ab931ca5dd0f6004c96de6cbd574
taivop <taivo.pungas@gmail.com>
2016-09-25 12:13:38 +0200

    implement Runnable instead of extending Thread

--- src/main/java/asl/MiddlewareComponentWriteThread.java
+++ src/main/java/asl/MiddlewareComponentWriteThread.java
@@ -6,12 +6,12 @@ import org.apache.logging.log4j.Logger;
 /**
  * This class is responsible for writing values to memcached and returning responses to the client.
  */
-public class MiddlewareComponentWriteThread extends Thread {
+public class MiddlewareComponentWriteThread implements Runnable {
 
     private static final Logger log = LogManager.getLogger(MiddlewareComponentWriteThread.class);
 
     @Override
     public void run() {
-
+        // TODO
     }
 }
