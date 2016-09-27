c845c4d307baaaa99e540313547e4f01ce4b5c62
taivop <taivo.pungas@gmail.com>
2016-09-27 14:59:20 +0200

    now request key is parsed correctly (+ test for this)

---
+++ src/main/java/asl/RequestTest.java
@@ -0,0 +1,22 @@
+package main.java.asl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequestTest {
+    @Test
+    /**
+     * Test whether the key is parsed correctly.
+     */
+    public void getKey() throws Exception {
+
+        Request r1 = new Request("set mykey 0 60 5", null);
+        assert(r1.getKey().equals("mykey"));
+
+        Request r2 = new Request("get lel", null);
+        assert(r2.getKey().equals("lel"));
+
+    }
+
+}
