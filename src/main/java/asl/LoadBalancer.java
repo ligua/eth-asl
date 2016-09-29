4f0276e1a10768b9f42d9b96f248096ea68bf7d3
taivop <taivo.pungas@gmail.com>
2016-09-29 10:46:35 +0200

    now getting also responses to write requests, but some error exists #22

--- src/main/java/asl/LoadBalancer.java
+++ src/main/java/asl/LoadBalancer.java
@@ -3,16 +3,13 @@ package main.java.asl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * This is the class that takes all incoming requests, hashes them and forwards to the correct MiddlewareComponent(s).
@@ -20,16 +17,19 @@ import java.util.Set;
 public class LoadBalancer implements Runnable {
 
     private static final Logger log = LogManager.getLogger(LoadBalancer.class);
+    private static final String lineEnd = "\r\n";
     private static final String address = "localhost";
     private static final Integer port = 11212;
 
     private List<MiddlewareComponent> middlewareComponents;
     private Hasher hasher;
 
+    private Map<SelectionKey, String> writeRequestLineBuffer;
+
     LoadBalancer(List<MiddlewareComponent> middlewareComponents, Hasher hasher) {
         this.middlewareComponents = middlewareComponents;
         this.hasher = hasher;
-
+        this.writeRequestLineBuffer = new HashMap<>();
     }
 
     /**
@@ -103,15 +103,48 @@ public class LoadBalancer implements Runnable {
                         client.read(buffer);
                         String message = new String(buffer.array()).trim();
 
-                        RequestType requestType = Request.getRequestType(message);
-                        log.debug(requestType + " message received: " + message);
+                        log.debug("receive from " + myKey);
+
+                        // RequestType requestType = Request.getRequestType(message);
+                        // log.debug(requestType + " message received: " + message);
+
+                        if(writeRequestLineBuffer.containsKey(myKey)) {
+                            // If we already have the whole SET request, we can create a Request.
+                            String fullMessage = writeRequestLineBuffer.get(myKey) + lineEnd + message + lineEnd;
+                            writeRequestLineBuffer.remove(myKey);
+                            Request r = new Request(fullMessage, client);
+                            log.debug(r.getType() + " message received: " + r.getRequestRaw());
+                            handleRequest(r);
+                        } else {
+                            RequestType requestType = Request.getRequestType(message);
+
+                            if(requestType == RequestType.GET || requestType == RequestType.DELETE) {
+                                Request r = new Request(message, client);
+                                log.debug(r.getType() + " message received: " + r.getRequestRaw());
+                                handleRequest(r);
+                            } else if(requestType == RequestType.SET) {
+                                // We need to wait for the second line in the SET request.
+                                writeRequestLineBuffer.put(myKey, message);
+                                log.debug("Got first line of SET request [" + message + "], waiting for second.");
+                            }
+
+                        }
+                        /*
                         if(requestType == RequestType.GET || requestType == RequestType.DELETE) {
                             Request r = new Request(message, client);
                             handleRequest(r);
                         } else if(requestType == RequestType.SET) {
-                            // We need to read one more line.
-                            // TODO can I block and wait here for more input? Is that a good idea?
-                        }
+                            if(!writeRequestLineBuffer.containsKey(myKey)) {
+                                // If we have just one line, we need to wait for the second line.
+                                writeRequestLineBuffer.put(myKey, message);
+                            } else {
+                                // If we already have the whole SET request, we can create a Request.
+                                String fullMessage = writeRequestLineBuffer.get(myKey) + message;
+                                log.debug(requestType + " message received: " + message);
+                                Request r = new Request(message, client);
+                                handleRequest(r);
+                            }
+                        }*/
                     }
                     selectionKeyIterator.remove();
                 }
