4c0530f229bbc0f56d3724d3b8d4b0d8c0218bd7
taivop <taivo.pungas@gmail.com>
2016-09-29 14:37:21 +0200

    client script update

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,6 +1,6 @@
 echo "Starting a memaslap client."
-mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 1 -c 1 -o1 -S 1s -t 20s -F resources/client_workload.config
+mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 2 -c 2 -o1 -S 1s -t 20s -F resources/client_workload.config
 
 # mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 1 -c 1 -o1 -S 1s -x 10 -F resources/client_workload.config
-# mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o1 -S 1s -t 10s -F conf/client_workload.config
+# mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o1 -S 1s -t 1m -F resources/client_workload.config
 # -s <server>:11212 -T 64 -c 64 -o1 -S 1s -t <time> -F <workloadconfig>
