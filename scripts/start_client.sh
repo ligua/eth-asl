635f413a9484e0d6d4ab7bbb384d9185ea84d0bf
taivop <taivo.pungas@gmail.com>
2016-09-30 12:56:13 +0200

    improve client start script

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,7 +1,18 @@
 echo "Starting a memaslap client."
 
-mem/libmemcached-1.0.18/clients/memaslap –s localhost:11212 –T 64 –c 64 –o1 –S 1s –t 1s –F resources/smallvalue.cfg
+# Official way, 1s
+#mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o1 -S 1s -t 1s –F resources/smallvalue.cfg
 
+# Official way, longer
+# mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o1 -S 1s -t 20s –F resources/smallvalue.cfg
+
+# Just 2 request generators
+mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 2 -c 2 -o1 -S 1s -t 20s –F resources/smallvalue.cfg
+
+
+
+
+# Graveyard
 # mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 1 -c 1 -o1 -S 1s -x 10 -F resources/client_workload.config
 # mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o1 -S 1s -t 1m -F resources/client_workload.config
 # -s <server>:11212 -T 64 -c 64 -o1 -S 1s -t <time> -F <workloadconfig>
