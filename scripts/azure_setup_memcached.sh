9dfbe87f66022628a2333513dbceb91abf7262f6
taivop <taivo.pungas@gmail.com>
2016-10-04 15:11:28 +0200

    memcached script

--- scripts/azure_setup_memcached.sh
+++ scripts/azure_setup_memcached.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 #TODO take as argument the ip of machine where I want to set up memcached
+username=$1
+machine_address=$2
+port=$3
 
+if [ -z $username ] || [ -z $machine_address ] || [ -z $port ]
+then
+echo "Use arguments: username machine_address port"
+exit 1
+fi
 
-# TODO update, install memcached, ...
+ssh $username@$machine_address "
+    echo '--- Updating ---'
+    sudo apt-get update
+    echo '--- Installing required libraries ---'
+    sudo apt-get install build-essential libevent-dev memcached
+    echo '--- Starting memcached ---'
+    memcached -p $port -t 1
+"
