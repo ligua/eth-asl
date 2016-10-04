16c13f8be491a7b93e89ab0564fd273b977dceb3
taivop <taivo.pungas@gmail.com>
2016-10-04 14:45:18 +0200

    azure script dummy files

---
+++ scripts/azure_setup_client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#TODO take as argument the ip of machine where I want to set up client
+
+
+# TODO update, install memcached, ...
---
+++ scripts/azure_setup_middleware.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#TODO take as argument the ip of machine where I want to set up middleware, and all information I need for starting middlewaremain
+
+
+# TODO update, install java, upload jar, start middleware
