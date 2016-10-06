3246f739c332e0e2ea62a7ca7f2a25f1392a6e96
taivop <taivo.pungas@gmail.com>
2016-10-06 15:07:16 +0200

    progress with fabric but still asks for password

--- scripts/py/test_small.py
+++ scripts/py/test_small.py
@@ -3,8 +3,8 @@ from haikunator import Haikunator
 from deployer import Deployer
 
 # region ---- Parameters ----
-my_resource_group = 'azure-python-deployment-test-small-4'
-my_pub_ssh_key_path = os.path.expanduser('~/.ssh/id_rsa.pub')
+my_resource_group = 'azure-python-deployment-test-small-1'
+my_pub_ssh_key_path = '~/.ssh/id_rsa.pub'
 template_path = "azure-templates/template3vms.json"
 dns_label_prefix = "pungast" + Haikunator().haikunate()
 
