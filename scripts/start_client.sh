c37510c03a17a28037b9103460b9e40b56f795a7
taivop <taivo.pungas@gmail.com>
2016-10-09 12:43:00 +0200

    fix character (dash) problems

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -1,13 +1,13 @@
 echo "Starting a memaslap client."
 
 # Official way, 1s
-#mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o0.9 -S 1s -t 1s –F resources/smallvalue.cfg
+#mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o0.9 -S 1s -t 1s -F resources/smallvalue.cfg
 
 # Official way, longer
-# mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o0.9 -S 1s -t 20s –F resources/smallvalue.cfg
+# mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o0.9 -S 1s -t 20s -F resources/smallvalue.cfg
 
 # Just 2 request generators
-mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 2 -c 2 -o0.9 -S 1s -t 1s –F resources/smallvalue.cfg
+mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 2 -c 2 -o0.9 -S 1s -t 1s -F resources/smallvalue.cfg
 
 
 
