f9198be655e925d93d89b540770ab43d74559b57
taivop <taivo.pungas@gmail.com>
2016-10-12 18:00:12 +0200

    test local now starts two memcacheds

--- scripts/test_local.sh
+++ scripts/test_local.sh
@@ -3,8 +3,10 @@
 LOG_DIR=tmp/local
 mkdir -p $LOG_DIR
 
-memcached -p 11211 -t 1 > $LOG_DIR/memcached.out 2>&1 &
-MEMCACHED_PID=$!
+memcached -p 11211 -t 1 > $LOG_DIR/memcached1.out 2>&1 &
+MEMCACHED_PID1=$!
+memcached -p 11210 -t 1 > $LOG_DIR/memcached2.out 2>&1 &
+MEMCACHED_PID2=$!
 
 sleep 2s
 
@@ -16,13 +18,14 @@ sleep 5s
 mem/libmemcached-1.0.18/clients/memaslap -s 127.0.0.1:11212 -T 64 -c 64 -o0.9 -S 1s -t 10s -F resources/xlargevalue.cfg > $LOG_DIR/memaslap.out &
 MEMASLAP_PID=$!
 
-echo "memcached $MEMCACHED_PID, middleware $MIDDLEWARE_PID, memaslap $MEMASLAP_PID"
+echo "memcached $MEMCACHED_PID1 $MEMCACHED_PID2, middleware $MIDDLEWARE_PID, memaslap $MEMASLAP_PID"
 
 sleep 20s
 
 echo `tail -n 1 $LOG_DIR/memaslap.out` | grep --color -e 'TPS: \d\{1,10\}'
 
 kill $MEMASLAP_PID
-kill $MEMCACHED_PID
+kill $MEMCACHED_PID1
+kill $MEMCACHED_PID2
 pkill -f java
 pkill -f ant
