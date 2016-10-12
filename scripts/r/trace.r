77af57a45e14aced5459b21001517abbf73fa86c
taivop <taivo.pungas@gmail.com>
2016-10-12 11:33:10 +0200

    trace analysis

--- scripts/r/trace.r
+++ scripts/r/trace.r
@@ -9,6 +9,7 @@ source("scripts/r/common.r")
 
 requests <- read.csv("results/trace/request.log", header=TRUE, sep=",")
 
+# ---- Distribution of response times
 data1 <- requests %>%
   mutate(dtAll=timeReturned-timeCreated) %>%
   select(dtAll)
@@ -19,3 +20,18 @@ g1 = ggplot(data1, aes(x=dtAll)) +
   ylab("Proportion of requests") +
   asl_theme
 ggsave("results/trace/graphs/dist_tAll.svg", g1, width=8, height=5)
+
+
+# ---- Throughput over time
+options(digits.secs=3)
+origin <- ISOdatetime(1970,1,1,0,0,0)
+data2 <- requests %>%
+  select(timeReturned) %>%
+  mutate(timeReturned=origin+timeReturned/1000) %>%
+  mutate(timeReturned=as.numeric(difftime(timeReturned, min(timeReturned), units="mins")))
+
+ggplot(data2, aes(x=timeReturned)) +
+  geom_histogram(binwidth=0.1) +
+  xlab("Time since the first request was returned (min)") +
+  ylab("100 requests") +  # TODO
+  asl_theme
