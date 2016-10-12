d188f9e549c9982cf06bd69d26305038a125eccb
taivop <taivo.pungas@gmail.com>
2016-10-12 16:00:46 +0200

    progress saving

--- src/main/java/asl/WriteWorker.java
+++ src/main/java/asl/WriteWorker.java
@@ -3,8 +3,14 @@ package main.java.asl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 
 /**
@@ -17,16 +23,48 @@ class WriteWorker implements Runnable {
     private Integer componentId;
     private List<Integer> targetMachines;
     private BlockingQueue<Request> writeQueue;
-    private List<MemcachedConnection> connections;
+    private List<Queue<Request>> serverQueues;
+    private List<SocketChannel> serverSockets;
 
     WriteWorker(Integer componentId, List<Integer> targetMachines, BlockingQueue<Request> writeQueue) {
         this.componentId = componentId;
         this.targetMachines = targetMachines;
         this.writeQueue = writeQueue;
+        this.serverQueues = new ArrayList<>();
+        this.serverSockets = new ArrayList<>();
 
-        connections = new ArrayList<>();
-        for(Integer targetMachine : targetMachines) {
-            connections.add(new MemcachedConnection(targetMachine));
+        try {
+            for (Integer targetMachine : targetMachines) {
+                serverQueues.add(new LinkedList<Request>());
+
+                // TODO open connection to memcached server
+                String addressString = MiddlewareMain.memcachedAddresses.get(targetMachine);
+                String[] parts = addressString.split(":");
+                String address = parts[0];
+                Integer port = Integer.parseInt(parts[1]);
+                InetSocketAddress inetSocketAddress = new InetSocketAddress(address, port);
+
+                SocketChannel socketChannel = SocketChannel.open(inetSocketAddress);
+                socketChannel.configureBlocking(false);
+
+                this.serverSockets.add(socketChannel);
+            }
+
+            // Wait for connection to all servers to finish
+            List<SocketChannel> notFinishedYet = new ArrayList<>(serverSockets);
+            while (notFinishedYet.size() > 0) {
+                for(SocketChannel socketChannel : notFinishedYet) {
+                    if (socketChannel.finishConnect()) {
+                        notFinishedYet.remove(socketChannel);
+                    }
+                }
+            }
+
+
+
+        } catch (Exception ex) {
+            log.error(ex);
+            throw new RuntimeException(ex);
         }
     }
 
@@ -36,23 +74,23 @@ class WriteWorker implements Runnable {
             log.info(String.format("%s started; writing to machines: %s.", getName(), Util.collectionToString(targetMachines)));
 
             while (true) {
+                serverSockets.get(0);
+
                 if (!writeQueue.isEmpty()) {
-                    try {
-                        Request r = writeQueue.take();
-                        r.setTimeDequeued();
-                        log.debug(getName() + " processing request " + r);
+                    Request r = writeQueue.take();
+                    r.setTimeDequeued();
+                    log.debug(getName() + " processing request " + r);
 
-                        // Write to all secondary machines
-                        for(MemcachedConnection mc : connections.subList(1, connections.size())) {
-                            mc.sendRequest(r, false);
+                    for(Integer targetMachine : targetMachines) {
+                        SocketChannel socketChannel = serverSockets.get(targetMachine);
+                        ByteBuffer buffer = r.getBuffer();
+                        while(buffer.hasRemaining()) {
+                            socketChannel.write(buffer);
                         }
+                        buffer.position(0);
 
-                        // Write to primary machine
-                        connections.get(0).sendRequest(r);
-
-                    } catch (InterruptedException ex) {
-                        log.error(ex);
                     }
+
                 }
             }
         } catch (Exception ex) {
