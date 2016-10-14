8ade1774d605e622bb2e4f8865920e44fbc53d75
taivop <taivo.pungas@gmail.com>
2016-10-14 13:23:24 +0200

    tiny change in default filename

--- scripts/py/extractor.py
+++ scripts/py/extractor.py
@@ -151,4 +151,4 @@ class Extractor:
 
 if __name__ == "__main__":
     e = Extractor()
-    e.summarise_trace_logs(logs_pattern="results/trace2/memaslap*.out", csv_path="results/trace2/memaslap_stats.csv")
+    e.summarise_trace_logs(logs_pattern="results/trace/memaslap*.out", csv_path="results/trace/memaslap_stats.csv")
