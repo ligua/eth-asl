09bc4c17ff07e87fcae50765f4afef9681102000
taivop <taivo.pungas@gmail.com>
2016-10-14 13:41:03 +0200

    trace analysis

--- scripts/r/trace.r
+++ scripts/r/trace.r
@@ -9,17 +9,22 @@ library(ggplot2, lib.loc=library_location)
 source("scripts/r/common.r")
 
 requests <- read.csv("results/trace/request.log", header=TRUE, sep=",")
+memaslap <- read.csv("results/trace/memaslap_stats.csv", header=TRUE, sep=";") %>%
+  mutate(min=min/1000, max=max/1000, avg=avg/1000, std=std/1000)
 result_dir_base <- "results/trace"
 
-# ---- Filter out beginning and end
-CUT_BEGINNING = 2 # minutes
-CUT_END = 1 # minutes
-min_time = min(requests$timeReturned) + 1e3 * 60 * CUT_BEGINNING
-max_time = max(requests$timeReturned) - 1e3 * 60 * CUT_END
-requests <- requests %>%
-  filter(timeReturned > min_time & timeReturned < max_time)
+# ---- Filter out beginning and end ----
+SHOULD_FILTER = FALSE
+if(SHOULD_FILTER) {
+  CUT_BEGINNING = 2 # minutes
+  CUT_END = 1 # minutes
+  min_time = min(requests$timeReturned) + 1e3 * 60 * CUT_BEGINNING
+  max_time = max(requests$timeReturned) - 1e3 * 60 * CUT_END
+  requests <- requests %>%
+    filter(timeReturned > min_time & timeReturned < max_time)
+}
 
-# ---- Distribution of response times
+# ---- Distribution of response times -----
 data1 <- requests %>%
   mutate(dtAll=timeReturned-timeCreated) %>%
   select(dtAll, type)
@@ -33,36 +38,38 @@ g1 = ggplot(data1, aes(x=dtAll)) +
 ggsave(paste0(result_dir_base, "/graphs/dist_tAll.svg"), g1, width=8, height=5)
 
 
-# ---- Throughput over time
-options(digits.secs=3)
-origin <- ISOdatetime(1970,1,1,0,0,0)
-data2 <- requests %>%
-  select(timeReturned) %>%
-  mutate(timeReturned=origin+timeReturned/1000) %>%
-  mutate(timeReturned=as.numeric(difftime(timeReturned, min(timeReturned), units="mins")))
+# ---- Throughput over time ----
+data2 <- memaslap %>%
+  filter(type=="t") %>%
+  group_by(time) %>%
+  summarise(tps=sum(tps))
 
-
-num_bins = 60
-h <- hist(data2$timeReturned, plot=FALSE, breaks=num_bins)
-binwidth_in_minutes = h$breaks[2] - h$breaks[1]
-tps_values <- h$counts / binwidth_in_minutes / 60 * 100
-
-data2p <- data.frame(h$mids, tps_values) %>%
-  rename(minute=h.mids)
-g2 <- ggplot(data2p) +
-  geom_line(aes(x=minute, y=tps_values), color=color_dark, size=2) +
-  xlab("Time since start of experiment (min)") +
-  ylab("Throughput (requests / s)") +
+g2 <- ggplot(data2) +
+  geom_line(aes(x=time, y=tps), color=color_dark, size=2) +
+  xlab("Time since start of experiment (s)") +
+  ylab("Total throughput (requests / s)") +
   asl_theme
 ggsave(paste0(result_dir_base, "/graphs/throughput.svg"), g2, width=8, height=5)
 
 
+# ---- Latency over time ----
+data3 <- memaslap %>%
+  filter(type=="t") %>%
+  group_by(time, request_type) %>%
+  summarise(avg=sum(avg * ops) / sum(ops))
 
+g3 <- ggplot(data3) +
+  geom_line(aes(x=time, y=avg, color=request_type), size=2) +
+  xlab("Time since start of experiment (s)") +
+  ylab("Latency measured by memaslap (ms)") +
+  facet_wrap(~request_type, nrow=2, scales="free") +
+  asl_theme
+ggsave(paste0(result_dir_base, "/graphs/latency"), g2, width=8, height=5)
 
 #ggsave(paste0(result_dir_base, "/graphs/throughput_over_time.svg"), g2, width=8, height=5)
 
 # ---- Time spent in different parts of the system
-data3 <- requests %>%
+data4 <- requests %>%
   mutate(tLoadBalancer=timeEnqueued-timeCreated,
          tQueue=timeDequeued-timeEnqueued,
          tWorker=timeForwarded-timeDequeued,
@@ -70,10 +77,10 @@ data3 <- requests %>%
   select(type, tLoadBalancer:tMemcachedAndReturn) %>%
   melt(id.vars=c("type"))
 
-g3 <- ggplot(data3) +
+g4 <- ggplot(data4) +
   geom_histogram(aes(x=value, fill=type)) +
   facet_wrap(~variable + type, ncol=2, scales="free") +
   xlab("Time spent (ms)") +
   ylab("Number of requests") +
   asl_theme
-ggsave(paste0(result_dir_base, "/graphs/time_breakdown.svg"), g3, width=8, height=5)
+ggsave(paste0(result_dir_base, "/graphs/time_breakdown.svg"), g4, width=8, height=5)
