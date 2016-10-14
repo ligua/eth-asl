5ba8ca0a47ca9672b3a92c17a20619ec2997ce72
taivop <taivo.pungas@gmail.com>
2016-10-14 13:09:10 +0200

    extractor works nicely on extracting running stats

--- scripts/py/extractor.py
+++ scripts/py/extractor.py
@@ -4,7 +4,8 @@ import re
 
 class Extractor:
 
-    re_filename = re.compile(r".*\/baseline_memaslap(\d)_conc(\d{3})_rep(\d{2}).out")
+    re_baseline_memaslap_filename = re.compile(r".*\/baseline_memaslap(\d)_conc(\d{3})_rep(\d{2}).out")
+    re_trace_memaslap_filename = re.compile(r".*\/memaslap(\d).out")
     re_total_events = re.compile(r"Total Statistics \((\d+) events\)")
     re_last_line = re.compile(r"Run time: (\d+\.\d+)s Ops: (\d+) TPS: (\d+) Net_rate: (\d+\.\d+)")
     re_min = re.compile(r"\s*Min:\s*(\d+)\s*")
@@ -12,6 +13,11 @@ class Extractor:
     re_avg = re.compile(r"\s*Avg:\s*(\d+)\s*")
     re_geo = re.compile(r"\s*Geo:\s*(\d+)\s*")
     re_std = re.compile(r"\s*Std:\s*(\d+)\s*")
+    re_get_statistics = re.compile(r"^Get Statistics$")
+    re_set_statistics = re.compile(r"^Set Statistics$")
+    re_total_statistics = re.compile(r"^Total Statistics$")
+    re_period_stats = re.compile(r"^Period\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\.\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+\.\d+)\s+(\d+\.\d+)\s*")
+    re_global_stats = re.compile(r"^Global\s+(\d+).*")
 
     DELIMITER = ";"
 
@@ -24,15 +30,15 @@ class Extractor:
             csv_writer.writerow(["filename", "client_no", "concurrency", "repetition", "total_events", "run_time",
                                      "ops", "tps", "net_rate", "tmin", "tmax", "tavg", "tgeo", "tstd"])
 
-            total_line_passed = False
-
             filenames = glob.glob(logs_pattern)
             for filename in filenames:
+                total_line_passed = False
+
                 with open(filename) as log_file:
 
                     print(filename)
 
-                    groups = Extractor.re_filename.search(filename).groups()
+                    groups = Extractor.re_baseline_memaslap_filename.search(filename).groups()
                     client_no = int(groups[0])
                     concurrency = int(groups[1])
                     repetition = int(groups[2])
@@ -64,6 +70,57 @@ class Extractor:
                 csv_writer.writerow(row)
 
 
+    @staticmethod
+    def summarise_trace_logs(logs_pattern="results/trace/memaslap*.out", csv_path="results/trace/memaslap_stats.csv"):
+        with open(csv_path, "w") as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=Extractor.DELIMITER)
+
+            # Header
+            csv_writer.writerow(["memaslap_number", "type", "request_type", "time", "ops", "tps", "get_misses", "min", "max", "avg", "std"])
+
+            filenames = glob.glob(logs_pattern)
+            for filename in filenames[0:1]:
+                total_line_passed = False
+                memaslap_number = Extractor.re_trace_memaslap_filename.search(filename).groups()[0]
+
+                with open(filename) as log_file:
+
+                    for line in log_file:
+                        if total_line_passed:
+                            type = "global"
+                        else:
+                            type = "t"
+
+                        if Extractor.re_total_events.match(line):
+                            total_events = Extractor.re_total_events.search(line).group(1)
+                            total_line_passed = True
+                        elif Extractor.re_get_statistics.match(line):
+                            request_type = "GET"
+                        elif Extractor.re_set_statistics.match(line):
+                            request_type = "SET"
+                        elif Extractor.re_total_statistics.match(line):
+                            request_type = None
+                        elif Extractor.re_period_stats.match(line):
+                            s = Extractor.re_period_stats.search(line).groups()
+                            ops = s[1]
+                            tps = s[2]
+                            get_misses = s[4]
+                            min = s[5]
+                            max = s[6]
+                            avg = s[7]
+                            std = s[8]
+                        elif Extractor.re_global_stats.match(line) and request_type:
+                            s = Extractor.re_global_stats.search(line).groups()
+                            time = s[0]
+                            row = [memaslap_number, type, request_type, time, ops, tps, get_misses, min, max, avg, std]
+                            csv_writer.writerow(row)
+
+
+                    print(filename)
+
+
+
+
 
 
 
@@ -71,4 +128,4 @@ class Extractor:
 
 if __name__ == "__main__":
     e = Extractor()
-    e.summarise_baseline_logs(logs_pattern="results/baseline/*.out")
+    e.summarise_trace_logs(logs_pattern="results/trace2/memaslap*.out", csv_path="results/trace2/memaslap_stats.csv")
