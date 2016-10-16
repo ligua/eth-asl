1e4e54fb90b622027bd36b01a45b71a33cfbaec1
taivop <taivo.pungas@gmail.com>
2016-10-16 22:39:05 +0200

    include graphs in report automatically based on path

--- scripts/r/trace.r
+++ scripts/r/trace.r
@@ -8,6 +8,8 @@ library(ggplot2, lib.loc=library_location)
 
 source("scripts/r/common.r")
 
+FIGURE_TYPE = ".pdf"
+SHOULD_FILTER = FALSE
 
 
 # ---- Parse command line args ----
@@ -25,7 +27,6 @@ memaslap <- read.csv(paste0(result_dir_base, "/memaslap_stats.csv"), header=TRUE
   mutate(min=min/1000, max=max/1000, avg=avg/1000, std=std/1000)
 
 # ---- Filter out beginning and end ----
-SHOULD_FILTER = FALSE
 if(SHOULD_FILTER) {
   CUT_BEGINNING = 2 # minutes
   CUT_END = 1 # minutes
@@ -46,7 +47,8 @@ g1 = ggplot(data1, aes(x=dtAll)) +
   xlab("Time from receiving request to responding (ms)") +
   ylab("Proportion of requests") +
   asl_theme
-ggsave(paste0(result_dir_base, "/graphs/dist_tAll.svg"), g1, width=8, height=5)
+ggsave(paste0(result_dir_base, "/graphs/dist_tAll", FIGURE_TYPE), g1,
+       width=fig_width, height=fig_height, device=cairo_pdf)
 
 
 # ---- Throughput over time ----
@@ -60,7 +62,8 @@ g2 <- ggplot(data2) +
   xlab("Time since start of experiment (s)") +
   ylab("Total throughput (requests / s)") +
   asl_theme
-ggsave(paste0(result_dir_base, "/graphs/throughput.svg"), g2, width=10, height=5)
+ggsave(paste0(result_dir_base, "/graphs/throughput", FIGURE_TYPE), g2,
+       width=fig_width, height=fig_height, device=cairo_pdf)
 
 
 # ---- Latency over time ----
@@ -77,17 +80,19 @@ data3summarised <- memaslap %>%
 g3 <- ggplot(data3) +
   geom_line(aes(x=time, y=avg, ymin=0, color=request_type), size=2) +
   xlab("Time since start of experiment (s)") +
-  ylab("Latency measured by memaslap (ms)") +
+  ylab("Response time measured by memaslap (ms)") +
   facet_wrap(~request_type, nrow=2, scales="free") +
   asl_theme
-ggsave(paste0(result_dir_base, "/graphs/latency_breakdown.svg"), g3, width=10, height=5)
+ggsave(paste0(result_dir_base, "/graphs/latency_breakdown", FIGURE_TYPE), g3,
+       width=fig_width, height=fig_height, device=cairo_pdf)
 
 g3summarised <- ggplot(data3summarised) +
   geom_line(aes(x=time, y=avg, ymin=0), color=color_dark, size=2) +
   xlab("Time since start of experiment (s)") +
-  ylab("Latency measured by memaslap (ms)") +
+  ylab("Response time measured by memaslap (ms)") +
   asl_theme
-ggsave(paste0(result_dir_base, "/graphs/latency.svg"), g3summarised, width=10, height=5)
+ggsave(paste0(result_dir_base, "/graphs/responsetime", FIGURE_TYPE), g3summarised,
+       width=fig_width, height=fig_height, device=cairo_pdf)
 
 # ---- Time spent in different parts of the system
 data4 <- requests %>%
@@ -105,4 +110,5 @@ g4 <- ggplot(data4) +
   xlab("Time spent (ms)") +
   ylab("Number of requests") +
   asl_theme
-ggsave(paste0(result_dir_base, "/graphs/time_breakdown.svg"), g4, width=10, height=14)
+ggsave(paste0(result_dir_base, "/graphs/time_breakdown", FIGURE_TYPE), g4,
+       width=fig_width, height=1.5*fig_width, device=cairo_pdf)
