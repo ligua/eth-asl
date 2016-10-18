b73a09e61fc758aceedb30de2415b5c60a3d889f
taivop <taivo.pungas@gmail.com>
2016-10-18 19:18:18 +0200

    update jar standalone test script

--- scripts/jar_standalone_test.sh
+++ scripts/jar_standalone_test.sh
@@ -5,4 +5,4 @@ cp dist/$NEWESTJAR ../test-jar
 echo "JAR contents:"
 jar -tf ../test-jar/$NEWESTJAR
 
-java -jar ../test-jar/$NEWESTJAR # run the latest-modified JAR
+java -jar ../test-jar/$NEWESTJAR -l 127.0.0.1 -p 11212 -t 5 -r 2 -m 127.0.0.1:11210 127.0.0.1:11211 # run the latest-modified JAR
