4979300fea81825d921660b775d5a2f2cb621310
taivop <taivo.pungas@gmail.com>
2016-10-18 14:43:27 +0200

    working on stdev questiojn

--- scripts/r/stdevs.r
+++ scripts/r/stdevs.r
@@ -15,20 +15,33 @@ requests <- read.csv("results/trace_rep3/request.log", header=TRUE, sep=",")
 # exp_end = min(requests$timeReturned)
 NUM_BUCKETS = 20
 
-h <- hist(requests$timeCreated, breaks=NUM_BUCKETS, plot=FALSE)
 requests2 <- requests %>%
   mutate(bucket = as.numeric(cut(requests$timeCreated, NUM_BUCKETS)),
          totalTime = timeReturned-timeCreated) %>%
-  select(totalTime, bucket) %>%
-  group_by(bucket) %>%
+  select(totalTime, bucket, type) %>%
+  group_by(bucket, type) %>%
   summarise(q01=quantile(totalTime, 0.1),
             q03=quantile(totalTime, 0.3),
             q05=quantile(totalTime, 0.5),
             q07=quantile(totalTime, 0.7),
-            q09=quantile(totalTime, 0.9)) %>%
-  melt(id.vars=c("bucket"))
+            q09=quantile(totalTime, 0.9),
+            mean=mean(totalTime),
+            std=sd(totalTime)) %>%
+  melt(id.vars=c("bucket", "type"))
 
+quantiles <- requests2 %>% filter(variable != "std" & variable != "mean")
+stds <- requests2 <- requests %>%
+  mutate(bucket = as.numeric(cut(requests$timeCreated, NUM_BUCKETS)),
+         totalTime = timeReturned-timeCreated) %>%
+  select(totalTime, bucket, type) %>%
+  group_by(bucket, type) %>%
+  summarise(mean=mean(totalTime),
+            std=sd(totalTime))
 
-ggplot(requests2) +
-  geom_line(aes(x=bucket, y=value, color=variable)) +
+ggplot() +
+  geom_ribbon(data=stds, aes(x=bucket, ymin=mean-std, ymax=mean+std),
+              fill="black", alpha=0.4) +
+  geom_line(data=quantiles, aes(x=bucket, y=value, color=variable), size=2) +
+  geom_line(data=stds, aes(x=bucket, y=mean), color="red", size=2) +
+  facet_wrap(~type) +
   asl_theme
