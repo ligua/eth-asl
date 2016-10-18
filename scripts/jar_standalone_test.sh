a22f615ba9c989cdc6a090c323f9ff448820d7a2
taivop <taivo.pungas@gmail.com>
2016-10-18 19:24:01 +0200

    run jar in the same dir where I create it

--- scripts/jar_standalone_test.sh
+++ scripts/jar_standalone_test.sh
@@ -1,8 +1,6 @@
-mkdir ../test-jar
 NEWESTJAR=`ls -Art dist/ | tail -n 1`
-cp dist/$NEWESTJAR ../test-jar
 
 echo "JAR contents:"
-jar -tf ../test-jar/$NEWESTJAR
+jar -tf dist/$NEWESTJAR
 
-java -jar ../test-jar/$NEWESTJAR -l 127.0.0.1 -p 11212 -t 5 -r 2 -m 127.0.0.1:11210 127.0.0.1:11211 # run the latest-modified JAR
+java -jar dist/$NEWESTJAR -l 127.0.0.1 -p 11212 -t 5 -r 2 -m 127.0.0.1:11210 127.0.0.1:11211 # run the latest-modified JAR
