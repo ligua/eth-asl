9f33645162432945a4d06bcec20cba1c85d70bac
taivop <taivo.pungas@gmail.com>
2016-10-21 10:58:24 +0200

    argument in report build script

--- report/build_report.sh
+++ report/build_report.sh
@@ -14,7 +14,7 @@ cp report/milestone1.pdf pungast-milestone1.pdf
 
 echo "Pushing to GitHub..."
 git add report/milestone1.pdf report/milestone1.tex pungast-milestone1.pdf
-git commit -m'auto-build report'
+git commit -m"auto-build report, $1"
 git push origin master
 
 echo "Pushing to GitLab..."
