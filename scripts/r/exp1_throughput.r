2a97957fb8401f11d660168c246bb78639e27309
taivop <taivo.pungas@gmail.com>
2016-11-06 13:23:20 +0100

    analysis for exp1

--- scripts/r/exp1_throughput.r
+++ scripts/r/exp1_throughput.r
@@ -1,1 +1,10 @@
 source("scripts/r/common.r")
+
+# ---- Parse command line args ----
+args <- commandArgs(trailingOnly=TRUE)
+if(length(args) == 1) {
+  result_dir_base <- args[1]
+} else {
+  stop("Arguments: [<results_directory>]")
+}
+
