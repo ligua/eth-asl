741b8fa111ce32e12bb9e53b0bf0af1bebccb6da
taivop <taivo.pungas@gmail.com>
2016-11-08 13:06:26 +0100

    fix #110 exp1 hypothesis commit

--- scripts/r/milestone2_hypothesis.r
+++ scripts/r/milestone2_hypothesis.r
@@ -5,19 +5,22 @@ FIGURE_TYPE = ".pdf"
 ms2_report_graphs = "report/figures"
 
 # ---- Experiment 1: Throughput ----
-clients = c(1, seq(50, 500, 50))
+clients = c(1, seq(50, 600, 60))
 tp_1client = 100
-tp_saturated = 10000
+tp_saturated = 17200
 throughput = c(tp_1client, c(0.5, 0.8, 0.88, 0.93, 0.97, 0.98, 0.99, 1, 0.95, 0.87)*tp_saturated)
 
 
 data1 <- data.frame(clients, throughput)
 
+best_num_clients <- (data1 %>%
+  arrange(desc(throughput)) %>%
+  top_n(1))$clients[1]
+
 g1 <- ggplot(data1, aes(x=clients, y=throughput)) +
   geom_line(color=color_dark) +
   geom_point(color=color_dark) +
   xlab("Number of clients") + ylab("Throughput [requests/s]") +
-  ggtitle("Hypothesis: shape of the throughput graph") +
   asl_theme
 ggsave(paste0(ms2_report_graphs, "/hypothesis_throughput", FIGURE_TYPE), g1,
        width=fig_width, height=fig_height / 2, device=cairo_pdf)
