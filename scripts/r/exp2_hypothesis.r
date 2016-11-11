85310e690015205c7649e87c7ea30d397583e935
taivop <taivo.pungas@gmail.com>
2016-11-11 12:38:51 +0100

    working on #121

---
+++ scripts/r/exp2_hypothesis.r
@@ -0,0 +1,24 @@
+source("scripts/r/common.r")
+
+FIGURE_TYPE = ".pdf"
+
+ms2_report_graphs = "report/figures"
+
+# ---- Experiment 2: Replication ----
+data1 <- read.csv("report/exp2_hypothesis.csv") %>%
+  mutate(replication_factor=get_replication_factor(servers, replication)) %>%
+  mutate(servers=paste0(servers, " servers"))
+
+g1 <- ggplot(data1, aes(x=replication_factor, y=responsetime, group=1)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~type+servers, scales="free_x", ncol=3) +
+  ylim(0, NA) +
+  ylab("Response time") +
+  xlab("Replication factor") +
+  asl_theme +
+  theme(axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+g1
+ggsave(paste0(ms2_report_graphs, "/hypothesis_replication", FIGURE_TYPE), g1,
+       width=fig_width, height=fig_height, device=cairo_pdf)
