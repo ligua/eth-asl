406e39d688d17e95f79fa19ec8762c6c27ff6758
taivop <taivo.pungas@gmail.com>
2016-11-23 14:13:32 +0100

    update hypothesis exp1

--- scripts/r/exp1_hypothesis.r
+++ scripts/r/exp1_hypothesis.r
@@ -8,18 +8,19 @@ ms2_report_graphs = "report/figures"
 clients = c(1, seq(50, 600, 60))
 tp_1client = 100
 tp_saturated = 17200
-throughput = c(tp_1client, c(0.5, 0.8, 0.88, 0.93, 0.97, 0.98, 0.99, 1, 0.95, 0.87)*tp_saturated)
+throughput = c(tp_1client, c(0.4, 0.7, 0.85, 0.93, 0.97, 0.98, 0.99, 1, 0.95, 0.87)*tp_saturated)
 
 
 data1 <- data.frame(clients, throughput)
 
-best_num_clients <- 110
+best_num_clients <- 200
 
 g1 <- ggplot(data1, aes(x=clients, y=throughput)) +
-  geom_vline(xintercept=best_num_clients, color=color_light, size=1) +
+  geom_vline(xintercept=best_num_clients, color=color_light, alpha=0.5, size=50) +
   geom_line(color=color_dark) +
   geom_point(color=color_dark) +
   xlab("Number of clients") + ylab("Throughput [requests/s]") +
   asl_theme
+g1
 ggsave(paste0(ms2_report_graphs, "/hypothesis_throughput", FIGURE_TYPE), g1,
        width=fig_width, height=fig_height / 2, device=cairo_pdf)
