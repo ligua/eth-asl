5f18b47f016ba376ee8f5c14acb53405369cd10f
taivop <taivo.pungas@gmail.com>
2016-12-14 16:58:19 +0100

    #173 model1 with different classes but fucking package cant do T servers

--- scripts/r/part3_network.r
+++ scripts/r/part3_network.r
@@ -1,23 +1,58 @@
 source("scripts/r/common.r")
 source("scripts/r/ms3_common.r")
 
+# ---- Helper function ----
+normalise_request_log_df <- function(df) {
+  
+  first_request_time <- min(df$timeCreated)
+  last_request_time <- max(df$timeCreated)
+  DROP_TIMES_BEFORE = first_request_time + 2 * 60 * 1000
+  DROP_TIMES_AFTER = last_request_time - 2 * 60 * 1000
+  
+  df2 <- df %>% filter(timeCreated > DROP_TIMES_BEFORE &
+                          timeCreated <= DROP_TIMES_AFTER)
+  return(df2)
+}
+
 # ---- Directories and files ----
 output_dir <- "results/analysis/part3_network"
-exp_dir <- "results/throughput/clients180_threads32_rep0"
+exp_dir <- "results/replication/S5_R1_rep5"
 memaslap_file <- paste0(exp_dir, "/memaslap_stats.csv")
 requests_file <- paste0(exp_dir, "/request.log")
 
 # ---- Reading data ----
 memaslap <- file_to_df(memaslap_file) %>% mutate(repetition=0)
 requests <- file_to_df(requests_file, sep=",") %>%
-  filter(type == "GET")
+  normalise_request_log_df()
 
 # ---- Preprocessing ----
 mss <- memaslap_summary(memaslap)
-mean_rt_memaslap <- mss$mean_response_time_get
-mean_rt_middleware <- mean(requests$timeReturned-requests$timeCreated)
-mean_network_delay <- (mean_rt_memaslap-mean_rt_middleware)/2
-mean_lb_time <- mean(requests$timeEnqueued-requests$timeCreated)
-mean_mwcomponent_time <- mean(requests$timeReturned-requests$timeDequeued)
+
+model_inputs <- function(requests, mss, request_type) {
+  
+  requests <- requests %>%
+    filter(type == request_type)
+  
+  res <- list()
+  res$type <- request_type
+  res$lb_time <- mean(requests$timeEnqueued-requests$timeCreated)
+  res$mwcomponent_time <- mean(requests$timeReturned-requests$timeDequeued)
+  
+  rt_middleware <- mean(requests$timeReturned-requests$timeCreated)
+  
+  if(request_type == "GET") {
+    rt_memaslap <- mss$mean_response_time_get
+  } else {
+    rt_memaslap <- mss$mean_response_time_set
+  }
+  
+  res$network_delay <- (rt_memaslap-rt_middleware)/2
+  
+  return(res)
+}
+inputs_get <- model_inputs(requests, mss, "GET") %>% as.data.frame()
+inputs_set <- model_inputs(requests, mss, "SET") %>% as.data.frame()
+inputs <- inputs_get %>% rbind(inputs_set)
 
 # ---- Actual results ----
+tps <- mss$tps_mean
