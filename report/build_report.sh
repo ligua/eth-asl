64185a54c4b721942852fe7bbe7c2a60bdfd6004
taivop <taivo.pungas@gmail.com>
2016-12-22 17:06:36 +0100

    update autobuild script

--- report/build_report.sh
+++ report/build_report.sh
@@ -1,16 +1,5 @@
-echo "Plotting trace (R=3) results..."
-Rscript scripts/r/trace.r results/trace_rep3
-
-echo "Plotting baseline results..."
-Rscript scripts/r/baseline.r results/baseline
-
-echo "Building report PDF..."
-cd report
-pdflatex milestone1.tex
-cd ..
-
 echo "Copying report to the right place..."
-cp report/milestone1.pdf pungast-milestone1.pdf
+cp report/milestone3.pdf pungast-milestone3.pdf
 
 echo "Pushing to GitHub..."
 git add report/milestone1.pdf report/milestone1.tex pungast-milestone1.pdf
