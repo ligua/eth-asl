9bb76c8caffb2d50f4f69f05db011caa21d49837
taivop <taivo.pungas@gmail.com>
2016-12-22 17:07:16 +0100

    update autobuild script

--- report/build_report.sh
+++ report/build_report.sh
@@ -2,7 +2,7 @@ echo "Copying report to the right place..."
 cp report/milestone3.pdf pungast-milestone3.pdf
 
 echo "Pushing to GitHub..."
-git add report/milestone1.pdf report/milestone1.tex pungast-milestone1.pdf
+git add report/milestone3.pdf report/milestone3.tex pungast-milestone3.pdf
 git commit -m"auto-build report, $1"
 git push origin master
 
